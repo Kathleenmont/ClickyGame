e137eb72d1b772123504c7f5829ec4bdbcd2556b
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-06-21 11:26:45 -0400

    move files to main folder

---
+++ build/precache-manifest.a74ac177527b014b6041def1a55c7b37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55760ee3836daa25a85e2220f746fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff1c617ba7e1e48301dc",
+    "url": "/static/css/main.6c8875dc.chunk.css"
+  },
+  {
+    "revision": "d2608c4f6ab882cf92ea",
+    "url": "/static/js/2.fc68dfc8.chunk.js"
+  },
+  {
+    "revision": "ff1c617ba7e1e48301dc",
+    "url": "/static/js/main.cd6c1312.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
